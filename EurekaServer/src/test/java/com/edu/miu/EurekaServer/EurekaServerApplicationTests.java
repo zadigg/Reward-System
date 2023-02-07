77f6406775e3f6629e66955a039cbb1601454bda
Tewodroes <teddymeselu@gmail.com>
2023-02-06 21:24:52 -0600

    Eureka Server

---
+++ EurekaServer/src/test/java/com/edu/miu/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.miu.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
