f051b743c78686bf4c3c472c3835d35ab61d6647
your name <simegnew62@gmail.com>
2023-02-07 14:46:40 -0600

    the controller is done

--- AvatarService/src/main/java/com/example/avatarservice/controller/AvatarController.java
+++ AvatarService/src/main/java/com/example/avatarservice/controller/AvatarController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/avatars")
 public class AvatarController {
@@ -15,31 +17,41 @@ public class AvatarController {
     private IAvatarService iAvatarService;
 
     @PostMapping("/add-avatars")
-    public ResponseEntity<?> addAvatar(@RequestBody Avatar avatar){
-        iAvatarService.addAvatar(avatar);
-        return new ResponseEntity<>(avatar, HttpStatus.CREATED);
+    public ResponseEntity<Avatar> createAvatar(@RequestBody Avatar avatar){
+        Avatar createdAvatar = iAvatarService.addAvatar(avatar);
+        return ResponseEntity.ok(createdAvatar);
     }
 
     @PutMapping("/{avatarId}")
-    public ResponseEntity<?> updateAvatar(@PathVariable long avatarId, @RequestBody Avatar avatar){
-        return ResponseEntity.ok(iAvatarService.updateAvatar(avatar));
+    public ResponseEntity<Avatar> updateAvatar(@PathVariable Long avatarId, @RequestBody Avatar avatar){
+        Avatar updatedAvatar = iAvatarService.updateAvatar(avatarId,avatar);
+        return ResponseEntity.ok(updatedAvatar);
     }
 
     @DeleteMapping("/remove/{avatarId}")
-    public ResponseEntity<?> deleteAvatar(@PathVariable Long avatarId){
-        iAvatarService.deleteAvatar(avatarId);
-        return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<String> deleteAvatar(@PathVariable Long avatarId){
+        boolean isDeleted= iAvatarService.deleteAvatar(avatarId);
+        if (!isDeleted){
+            return new ResponseEntity<>("Avatar with id= " +avatarId+" not found.",HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<String>("Avatar with id= "+avatarId+" is successfully deleted.",HttpStatus.OK);
     }
-    @GetMapping("/get")
+    @GetMapping
     public ResponseEntity<?> getAllAvatar(){
-        iAvatarService.getAllAvatar();
-        return new ResponseEntity<>(HttpStatus.OK);
+        List<Avatar> avatars = iAvatarService.getAllAvatars();
+        if(avatars.size()==0){
+            return new ResponseEntity<String>("There is no Avatar in the database.", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<List<Avatar>>(avatars, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getAvatarById(@PathVariable long id){
-        iAvatarService.getAvatar(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @GetMapping("/{avatarId}")
+    public ResponseEntity<?> getAvatarById(@PathVariable Long avatarId){
+        Avatar avatar = iAvatarService.getAvatarById(avatarId);
+        if (avatar==null){
+            return new ResponseEntity<String>("Avatar with id= "+avatarId+" not found.", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Avatar>(avatar, HttpStatus.OK);
     }
 
 }
