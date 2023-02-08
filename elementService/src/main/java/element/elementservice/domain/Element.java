07fe46591be9a1d552e4fdef89a6469f67040900
your name <simegnew62@gmail.com>
2023-02-08 01:13:17 -0600

    domain implementation is done!!

---
+++ elementService/src/main/java/element/elementservice/domain/Element.java
@@ -0,0 +1,18 @@
+package element.elementservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+@NoArgsConstructor
+@AllArgsConstructor
+public class Element {
+    @Id
+    private String elementId;
+    private String type;
+    private double price;
+}
