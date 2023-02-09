f45471c7586c92d7e10c47f5257148c14fd85e86
Tewodroes <teddymeselu@gmail.com>
2023-02-09 00:29:28 -0600

    API Gateway Service

---
+++ APIGatewayService/src/test/java/com/edu/miu/APIGatewayService/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.miu.APIGatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
