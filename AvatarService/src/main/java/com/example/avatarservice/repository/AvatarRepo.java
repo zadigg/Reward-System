3503ce3885bbb2fbf48e17111c2502af7f561171
zadigg <abelkibebe5@gmail.com>
2023-02-09 22:33:16 -0600

    Avatar Service Removed

--- AvatarService/src/main/java/com/example/avatarservice/repository/AvatarRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.avatarservice.repository;
-
-import com.example.avatarservice.domain.Avatar;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AvatarRepo extends MongoRepository<Avatar, Long> {
-}
